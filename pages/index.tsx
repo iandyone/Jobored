0f693bc5aa5e0cbbe0c0fac48804d604df341dc4
Andrei Bernatski <iandy1731@gmail.com>
2023-04-26 19:11:36 +0300

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '@/styles/Home.module.scss'
+
+export default function Home() {
+  return (
+    <h1>Hello, Next!</h1>
+  )
+}
