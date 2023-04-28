e6565c14a2576b2fdb06792642fc9c5c9b8f20cd
Andrei Bernatski <iandy1731@gmail.com>
2023-04-28 15:41:48 +0300

    fix: filters bar UI fixes

--- components/select.tsx
+++ components/select.tsx
@@ -2,42 +2,39 @@ import { useDispatchTyped, useSelectorTyped } from "@/hooks/redux";
 import { MouseEvent, FC, useState } from "react";
 import { setCategory } from "@/store/slices/filter-slice";
 import styles from "../styles/select.module.scss";
-import Image from "next/image";
-import arrow from "../public/filters-bar/arrow.svg";
-import arrowActive from "../public/filters-bar/arrow-active.svg";
 
 interface ISelect {
   className: string;
 }
 
 const Select: FC<ISelect> = ({ className }) => {
-  const [selectActivity, setSelectActivity] = useState(false);
   const [menuVisability, setMenuVisability] = useState(false);
-  const { category: selectValue, categories } = useSelectorTyped((store) => store.filters);
-  const selectClassName = selectActivity ? `${className} ${styles.select} ${styles.active}` : `${className} ${styles.select}`;
+  const { category, categories } = useSelectorTyped((store) => store.filters);
+
+  const selectClassName = category ? `${className} ${styles.select} ${styles.active}` : `${className} ${styles.select}`;
   const dispatch = useDispatchTyped();
-   
-  function selectOnClick(e: MouseEvent<HTMLDivElement>) {
-    setMenuVisability(!menuVisability);
-  }
 
   function optionOnClick(e: MouseEvent<HTMLElement>) {
     const target = e.target as HTMLElement;
     const element = target.closest("li");
     const category = element!.getAttribute("data-value");
+
     dispatch(setCategory(category!));
-    setSelectActivity(true);
     setMenuVisability(false);
   }
-  
+
   return (
-    <div className={selectClassName} onClick={selectOnClick}>
-      <span>{selectValue || 'Выберите отрасль'}</span>
-      <div className={styles.select__icon}>
-        <Image src={menuVisability ? arrowActive : arrow} alt='x-mark icon' />
+    <div className={`${selectClassName} ${menuVisability ? styles.selected : ""}`} onClick={() => setMenuVisability(!menuVisability)}>
+      <span>{category || "Выберите отрасль"}</span>
+      <div className={`${styles.select__icon} ${menuVisability ? styles.active : ""}`}>
+        <div className={styles.select__svg}>
+          <svg xmlns='http://www.w3.org/2000/svg' width='25' height='25' viewBox='0 0 256 256'>
+            <path d='M213.66,101.66l-80,80a8,8,0,0,1-11.32,0l-80-80A8,8,0,0,1,53.66,90.34L128,164.69l74.34-74.35a8,8,0,0,1,11.32,11.32Z'></path>
+          </svg>
+        </div>
       </div>
       <ul className={menuVisability ? `${styles.select__menu} + ${styles.active}` : styles.select__menu}>
-        {categories && categories.map(({ id, title }) => {
+        {categories.map(({ id, title }) => {
           return (
             <li className={styles.select__option} data-value={title} onClick={optionOnClick} key={id}>
               <span>{title}</span>
