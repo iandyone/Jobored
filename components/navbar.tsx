2c08a22f275169debc34fcff0b35498c0585c368
Andrei Bernatski <iandy1731@gmail.com>
2023-05-05 16:10:59 +0300

    fix: fix the navbar active link bug

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,24 +3,18 @@ import { useRouter } from "next/router";
 import styles from "../styles/navigation.module.scss";
 import Link from "next/link";
 
-const navigation = [
-  { id: 1, title: "Поиск Вакансий", path: "/", className: styles.navigation__link },
-  { id: 2, title: "Избранное", path: "/favorites", className: styles.navigation__link },
-];
-
 const Navbar: FC = () => {
   const { pathname } = useRouter();
 
   return (
     <nav className={styles.navigation}>
       <ul className={styles.navigation__links}>
-        {navigation.map(({ id ,title, path, className }) => {
-          return (
-            <li key={id} className={path === pathname ? `${className} ${styles.active}` : className}>
-              <Link href={path}>{title}</Link>
-            </li>
-          );
-        })}
+        <li className={!pathname.includes("favorites") ? `${styles.navigation__link} ${styles.active}` : styles.navigation__link}>
+          <Link href='/'>Поиск Вакансий</Link>
+        </li>
+        <li className={pathname.includes("favorites") ? `${styles.navigation__link} ${styles.active}` : styles.navigation__link}>
+          <Link href='/favorites'>Избранное</Link>
+        </li>
       </ul>
     </nav>
   );
